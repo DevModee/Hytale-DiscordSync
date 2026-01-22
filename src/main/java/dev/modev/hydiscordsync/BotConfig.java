b4eb075ed7f431b86f24bba42027fbb83f42e95e
DevModee <caelien2.0@gmail.com>
2026-01-21 23:46:37 -0300

    creada clase simple para definir la estructura de datos de la configuracion

---
+++ src/main/java/dev/modev/hydiscordsync/BotConfig.java
@@ -0,0 +1,8 @@
+package dev.modev.hydiscordsync;
+
+public class BotConfig {
+
+    public String botToken = "TOKEN_DEL_BOT";
+    public String channelId = "000000000000000000";
+    public String mensajeInicio = "El servidor está en línea.";
+}
